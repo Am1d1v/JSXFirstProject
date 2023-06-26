775905baf39a3a55615acf5b1ff56c8856bc154c
Am1d1v <124504237+Am1d1v@users.noreply.github.com>
2023-06-26 15:44:59 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 
 
 
+const Hi = <h1>Hi</h1>
+
 const Hello = /*#__PURE__*/ React.createElement("h1", null, "Hello");
 
 
 const container = document.getElementById("app");
 const root = ReactDOM.createRoot(container);
-root.render(Hello);
+//root.render(Hello);
+root.render(Hi);
+
+
+
